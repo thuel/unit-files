725026834166d1b61b6baaab4d32af7e855e1e87
Lukas Steffen <lukas@debian-develop.fritz.box>
2017-05-23 16:23:21 +0200

    debug rebootreminder.py check wasn't sucessfull

--- rebootreminder.py
+++ rebootreminder.py
@@ -9,7 +9,7 @@ import os, subprocess as sp
 admins = ["lukas@steffen-steffen.ch"]
 
 # get hostname and define subject and message of notification:
-hostname = sp.popen("/usr/bin/cat /etc/hostname", shell=True, stdout=sp.PIPE).communicate()
+hostname = sp.Popen("cat /etc/hostname", shell=True, stdout=sp.PIPE).communicate()[0].strip()
 subject = "The system %s has been rebooted." % hostname
 message = "Please log in to start essential services."
 
@@ -18,4 +18,7 @@ cmds = []
 for admin in admins:
     cmds.append('echo "%s" | mail -s "%s" %s' % (message, subject, admin))
     
-process = sp.Popen(cmds, stdout=sp.PIPE)
+for cmd in cmds:
+    sp.Popen(cmd, shell=True, stdout=sp.PIPE)
+
+
